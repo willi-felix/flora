b92715310db995e2c742062e5929193248f8d540
William <172239352+willi-felix@users.noreply.github.com>
2024-12-24 15:25:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ class Record(db.Model):
 
 def create_app():
     app = Flask(__name__)
-    app.config['SITE_NAME'] = os.getenv('SITE_NAME', 'DicGo')
+    app.config['SITE_NAME'] = os.getenv('SITE_NAME', 'Dicgo')
     app.config['SLOGAN'] = os.getenv('SLOGAN', 'Search & Learn Effortlessly')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLITE_CLOUD_DB_URI', 'sqlitecloud://cje5zuxinz.sqlite.cloud:8860/dicgo.sqlite?apikey=SMZSFhzb4qCWGt8VElvtRei2kOKYWEsC1BfInDcS1RE')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
